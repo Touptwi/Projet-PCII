bd3d43a71a272a554f78825aceec69ec10e2d896
Touptwi <touptwi@gmail.com>
2022-01-31 16:14:17 +0100

    Flower 1.0 -Initialisation

---
+++ Modele/Flower.java
@@ -0,0 +1,29 @@
+package Modele;
+
+import java.awt.*;
+
+public class Flower extends Thread {
+
+    private int durability = 10;
+    private Color color = Color.red;
+
+    public Flower() {
+    }
+
+    public void run() {
+
+        while(durability>0) {
+            durability--;
+            if(durability<5 && color == Color.red) {
+                color = Color.orange;
+            } else if (durability<3 && color == Color.orange){
+                color = Color.yellow;
+            }
+            try { Thread.sleep(100); } catch (Exception e) { e.printStackTrace(); }
+        }
+    }
+
+    public Color getColor() { return color; }
+
+    public int getDurability() { return durability; }
+}
