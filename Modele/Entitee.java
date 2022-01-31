30ddfe885b95247ea609b042ae86a1f541f197ee
Neylick <neylick@gmail.com>
2022-01-31 16:35:11 +0100

    Oubli

---
+++ Modele/Entitee.java
@@ -0,0 +1,8 @@
+package Modele;
+import Vue.Affichable;
+
+public abstract class Entitee 
+{
+	Affichable affichable;
+	public Affichable getAffichable() { return affichable; }
+}
---
+++ Vue/Affichable.java
@@ -0,0 +1,8 @@
+package Vue;
+
+import java.awt.Graphics;
+
+public interface Affichable 
+{
+	public void draw(Graphics g);
+}
