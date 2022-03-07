0d5a804188866de808aad14cd706ffe42be7db1e
Touptwi <touptwi@users.noreply.github.com>
2022-03-07 15:45:01 +0100

    Add takeRessource

--- Modele/Goblin.java
+++ Modele/Goblin.java
@@ -4,17 +4,28 @@ import Vue.IE_Goblin;
 import Vue.VueGoblin;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class Goblin extends EntiteeAvecInventaire implements Runnable
 {
+
+    Ressource ressource;
 
     public Goblin(Etat e, Point pos, Ressource r)
     {
         this.interface_e = new IE_Goblin(e, this);
         this.affichable = new VueGoblin(this);
         this.position = pos;
+        this.ressource = r;
         e.getGrille().setCase(pos, this);
         new Deplacement(this, pos, r.getPosition(),e.getGrille()).start();
+    }
+
+    /** Prendre la ressource si atteint */
+    public void takeRessource(ArrayList<Point> voisins) {
+        if(voisins.contains(ressource.getPosition())) {
+            this.addToInventaire(ressource);
+        }
     }
 
     @Override
