949fc2eb97c543498f1ecd47736da60681a09f6f
Touptwi <touptwi@users.noreply.github.com>
2022-03-21 16:41:42 +0100

    Add dynamic goblin's sprite

--- Vue/AffichageGoblin/VueGoblin.java
+++ Vue/AffichageGoblin/VueGoblin.java
@@ -1,6 +1,7 @@
 package Vue.AffichageGoblin;
 
 import Modele.Entitees.EntiteeAvecInventaire.EntieesDeplacable.Goblin;
+import Modele.Entitees.EntiteeAvecInventaire.Inventaire;
 import Vue.Affichable;
 import Vue.Affichage;
 
@@ -12,17 +13,22 @@ import java.io.IOException;
 
 public class VueGoblin implements Affichable {
 
-    private final Goblin goblin;
+    private Goblin goblin;
 
     private BufferedImage img;
+    private BufferedImage img_;
 
     public VueGoblin(Goblin gob){
         this.goblin = gob;
-        try { img = ImageIO.read(new File("Images/Goblin/Goblin.png"));}
+        try {
+            img_ = ImageIO.read(new File("Images/Goblin/Goblin_.png"));
+            img = ImageIO.read(new File("Images/Goblin/Goblin.png"));
+        }
         catch (IOException E) { E.printStackTrace(); }
     }
 
     public void draw(Graphics g, Affichage a){
-        g.drawImage(img,10, 20, a.fitX(80), a.fitY(80),null);
+        if(goblin.getInventaire().isVide()) {g.drawImage(img_, 10, 20, a.fitX(80), a.fitY(80), null);}
+        else                                {g.drawImage(img, 10, 20, a.fitX(80), a.fitY(80), null);}
     }
 }
