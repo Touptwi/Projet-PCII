2d3ea2b5db9094ad6a0c512d7df3811f8d863b45
Touptwi <touptwi@users.noreply.github.com>
2022-04-04 17:30:43 +0200

    Fixe Stone interface

--- Modele/Entites/Stone.java
+++ Modele/Entites/Stone.java
@@ -1,13 +1,26 @@
 package Modele.Entites;
 
 import Modele.Etat;
+import Vue.InterfaceEntite;
 import Vue.VueStone;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Stone extends Entite {
     
     public Stone(Etat _e, Point pos) {
+        this.interface_e = new InterfaceEntite() {
+            @Override
+            public JPanel getJPanel() {
+                return new JPanel();
+            }
+
+            @Override
+            public void mise_a_jour() {
+
+            }
+        };
         this.affichable = new VueStone(this);
         this.position = pos;
         _e.getGrille().setCase(pos, this);
